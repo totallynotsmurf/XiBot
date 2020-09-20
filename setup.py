e23d175162cd10398f7c0e357e9b2c8133df4b48
ClawmanCat <hhy.yhh@live.nl>
2020-09-20 20:32:28 +0200

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 import os
+import sys
 
 
+interp   = str(sys.executable)
 commands = [
-    'pip install python-telegram-bot',
-    'pip install textblob',
-    'python -m textblob.download_corpora'
+    '-m pip install python-telegram-bot',
+    '-m pip install textblob',
+    '-m textblob.download_corpora'
 ]
 
 
 for command in commands:
-    os.system(command)
+    os.system(interp + ' ' + command)
 
 print('All done.')
